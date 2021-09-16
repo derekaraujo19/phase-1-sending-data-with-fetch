25b56a60b11de736d2679ce7ab58ad7ad7ae19e0
Derek Araujo <derekaraujo19@gmail.com>
2021-09-16 00:07:28 -0400

    commit tree 3fe1da9f6d675d9a967a8eab8ca72aec191d6e63

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Add your code here
+// function called submitData ( userName, userEmail )
+  // fetch -
+
+
+
+
+  function submitData(userName, userEmail) {
+    return fetch('http://localhost:3000/users', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+      },
+      body: JSON.stringify({
+        name: userName,
+        email: userEmail,
+      })
+    })
+        .then(res => res.json())
+        .then(function(obj) {
+          const body = document.querySelector('body');
+          body.append(obj.id)
+        })
+        .catch(function(error) {
+          const message = error.message;
+          document.body.append(message);
+        })
+  }
+
